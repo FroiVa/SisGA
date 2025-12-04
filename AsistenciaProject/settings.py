f5f278cd314c943128726e1a66928d237691320b
Froilan <froilan.valido@gmail.com>
2025-12-04 14:43:33 -0500

    forms, views y templates

--- AsistenciaProject/settings.py
+++ AsistenciaProject/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 ]
 
 # En producción
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
